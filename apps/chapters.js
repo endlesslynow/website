5c8eac8faa8eb9b3db7674d586d762223fe106f9
endlesslynow <endlesslynow@gmail.com>
2024-11-30 16:43:51 +0100

    Update apps folder

---
+++ apps/chapters.js
@@ -0,0 +1,87 @@
+window.chapters = {
+    "XV": {
+        title: "Chapter XV",
+        words: `[ORG]lūdus
+[TRANS]school
+[ORG]magister
+[TRANS]teacher
+[ORG]discipulus
+[TRANS]student
+[ORG]virga
+[TRANS]rod
+[ORG]sella
+[TRANS]chair
+[ORG]janua
+[TRANS]door
+[ORG]vērum
+[TRANS]truth
+[ORG]tergum
+[TRANS]back
+[ORG]malum
+[TRANS]evil
+[ORG]lectulus
+[TRANS]couch
+[ORG]sevērus
+[TRANS]strict
+[ORG]tacitus
+[TRANS]silent
+[ORG]vērus
+[TRANS]true
+[ORG]posterior
+[TRANS]later
+[ORG]inferior
+[TRANS]lower
+[ORG]prior
+[TRANS]former
+[ORG]pūnīre
+[TRANS]to punish
+[ORG]considere
+[TRANS]to sit down
+[ORG]exclāmāre
+[TRANS]to cry out
+[ORG]dēsinere
+[TRANS]to cease
+[ORG]redire
+[TRANS]to return
+[ORG]reddere
+[TRANS]to give back
+[ORG]recitāre
+[TRANS]to recite
+[ORG]licēre
+[TRANS]to be permitted
+[ORG]sum
+[TRANS]I am
+[ORG]es
+[TRANS]you are
+[ORG]sumus
+[TRANS]we are
+[ORG]estis
+[TRANS]you all are
+[ORG]ego
+[TRANS]I
+[ORG]tū
+[TRANS]you
+[ORG]nōs
+[TRANS]we
+[ORG]vōs
+[TRANS]you all
+[ORG]nōndum
+[TRANS]not yet
+[ORG]statim
+[TRANS]immediately
+[ORG]tum
+[TRANS]then
+[ORG]quid
+[TRANS]what?
+[ORG]domī
+[TRANS]at home
+[ORG]antequam
+[TRANS]before
+[ORG]at
+[TRANS]but
+[ORG]sī
+[TRANS]if
+[ORG]nisi
+[TRANS]unless`
+    }
+};
